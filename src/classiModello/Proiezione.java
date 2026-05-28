540549fbaf34f930168ef1522933942ce027ddaf
Manuel <ernst.sgmmt@gmail.com>
2026-05-28 12:32:28 +0200

    proiezione astratta sistemata(in teoria)

--- src/classiModello/Proiezione.java
+++ src/classiModello/Proiezione.java
@@ -1,40 +1,102 @@
 package classiModello;
 
 import java.time.*;
+import java.util.Arrays;
 
 public abstract class Proiezione {
 
 	private static final String[] TAG_CONSENTITI= {"SERALE", "WEEKEND", "FAMIGLIE", "ANTEPRIMA", "LINGUA_ORIGINALE", "EVENTO", "TRE_D"};
 	
 	private static int identificativo = 0;
-	private int codiceFilm;
-	private int codiceSala;
-	private LocalDate dataProiezione;
+	private Film film;
+	private Sala sala;
+	private LocalDate data;
 	private LocalTime oraProiezione;
 	private double prezzoBase;
-	private String tags;
+	private String[] tags;
 	
-	public abstract 
 	
+	public Proiezione(Film film, Sala sala, LocalDate data, LocalTime oraProiezione, double prezzoBase, String[] tags) {
+		super();
+		this.film = film;
+		this.sala = sala;
+		this.data = data;
+		this.oraProiezione = oraProiezione;
+		this.prezzoBase = prezzoBase;
+		this.tags = tags;
+	}
+	
+	
+
+	public Film getFilm() {
+		return film;
+	}
+
+	public void setFilm(Film film) {
+		this.film = film;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public LocalTime getOraProiezione() {
+		return oraProiezione;
+	}
+
+	public void setOraProiezione(LocalTime oraProiezione) {
+		this.oraProiezione = oraProiezione;
+	}
+
+	public double getPrezzoBase() {
+		return prezzoBase;
+	}
+
+	public void setPrezzoBase(double prezzoBase) {
+		this.prezzoBase = prezzoBase;
+	}
+
+	public String[] getTags() {
+		return tags;
+	}
+
+	public void setTags(String[] tags) {
+		if (tags.equals(TAG_CONSENTITI)) {
+			this.tags = tags;
+		}
+	}
+
 	public abstract double calcoloPrezzoFinale();
 
 	public LocalDateTime getInizioProiezione() {		
-		return dataProiezione.atTime(oraProiezione);
+		return data.atTime(oraProiezione);
 	}
 	
 	public LocalDateTime getFineProiezione(Duration durata) {
-		return dataProiezione.atTime(oraProiezione.plus(durata));
+		return data.atTime(oraProiezione.plus(durata));
 	}
 	
 	public boolean IsToday() {
-		if (dataProiezione.isEqual(LocalDate.now()))
+		if (data.isEqual(LocalDate.now()))
 			return true;
 		else
 			return false;
 	}
 	
 	public boolean IsInWeekend() {
-		if (dataProiezione.getDayOfWeek().getValue() == 6 || dataProiezione.getDayOfWeek().getValue() == 7)
+		if (data.getDayOfWeek().getValue() == 6 || data.getDayOfWeek().getValue() == 7)
 		return true;
 			else
 		return false;
@@ -60,5 +122,10 @@ public abstract class Proiezione {
 		else
 			return false;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Proiezione [data=" + data + ", oraProiezione=" + oraProiezione + ", prezzoBase=" + prezzoBase
+				+ ", tags=" + Arrays.toString(tags) + "]";
+	}
 }
